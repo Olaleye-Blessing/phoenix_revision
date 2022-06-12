924719ec04888ddccb13f0e352dad857e6f831f5
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-06-12 21:17:47 +0100

    chapter 3

---
+++ test/support/fixtures/catalog_fixtures.ex
@@ -0,0 +1,28 @@
+defmodule Legend.CatalogFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Legend.Catalog` context.
+  """
+
+  @doc """
+  Generate a unique product sku.
+  """
+  def unique_product_sku, do: System.unique_integer([:positive])
+
+  @doc """
+  Generate a product.
+  """
+  def product_fixture(attrs \\ %{}) do
+    {:ok, product} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        name: "some name",
+        sku: unique_product_sku(),
+        unit_price: 120.5
+      })
+      |> Legend.Catalog.create_product()
+
+    product
+  end
+end
