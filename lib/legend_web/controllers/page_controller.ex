77a1892b45e14167fd3a56a12510e2ab608c4b2d
Olaleye Blessing <Olayinkablexxy@gmail.com>
2022-06-12 11:59:10 +0100

    chapter 2 task c

--- lib/legend_web/controllers/page_controller.ex
+++ lib/legend_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule LegendWeb.PageController do
   use LegendWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    redirect(conn, to: "/guess")
+    # render(conn, "index.html")
   end
 end
